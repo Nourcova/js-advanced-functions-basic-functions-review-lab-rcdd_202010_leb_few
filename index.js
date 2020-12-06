4845d971fbca86baa37b0bab7e457b3257f18450
Ibrahim Nour <ibrahamnour22@gmail.com>
2020-12-06 17:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(str="roller-skate"){
+  return `This Saturday, I want to ${str}!`;
+}
+function mondayWork(str="go to the office"){
+  return `This Monday, I will ${str}.`;
+}
+
+function wrapAdjective(flair="*"){
+  return function(str="special"){
+    return `${flair}You are ${str}${flair}`;
+  }
+}
+let Calculator={
+  add : function() {
+    return 1 + 3;
+}
